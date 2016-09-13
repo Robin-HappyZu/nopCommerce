1a9960d2e4c4ac260bea87fce3d77ceef0e1d90f
YeChun <YeChun@DESKTOP-NNJ2G7T>
2016-09-13 14:25:00 +0800

    Wechat站点插件

---
+++ src/Plugins/Happyzu.Plugin.Wechat.Web/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happyzu.Plugin.Wechat.Web.Controllers
+{
+    public class ProductController : BasePluginController
+    {
+
+    }
+}
